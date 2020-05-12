e03eb3b74260150a41c3c96bb679d7e78799bf08
Walker, Corey <corey_walker@mcafee.com>
2020-05-11 21:56:24 -0700

    create skeleton routes and models

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+
+router.post("/api/workouts", ({body}, res) => {
+
+});
+
+module.exports = router;
